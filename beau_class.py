f08f8d7cd6e0de74bc3595947f78f65ddf3bcb4b
sean.mccurdy@utoronto.ca <seanmccurdy@sean-mac.local>
2016-09-18 20:33:11 -0700

    added dual price/classification functionality; interacts with terminal

--- beau_class.py
+++ beau_class.py
@@ -7,6 +7,7 @@ import sys
 import matplotlib.pyplot as plt
 from pandas import DataFrame,Series
 from sqlalchemy import create_engine
+from sklearn.metrics import accuracy_score
 from sklearn.cross_validation import cross_val_score,KFold,train_test_split
 from sklearn.pipeline import Pipeline
 from beau_cred import *
@@ -20,20 +21,30 @@ os.chdir("/Users/seanmccurdy/Dropbox/insight_project/code")
 img_rows = 121
 img_cols = 91
 channels = 3
+image_shape = (channels,img_rows,img_cols)
 seed = 42
+target = sys.argv[1] ##alternatives are'prime_cat'(categorization)or'price' 
+### ^^^ performs regression ##takes from command line
 
 ######################
 ### ML Preparation ###
 ######################
 
-X, Y, L = load_processed_data(	X_filename = "../processed_data/images_RGB_121_91_expanded.npy",
-								Y_filename= "../processed_data/prime_categories_expanded.npy",
-								labels_filename = "../processed_data/labels_expanded.npy",
-								randomize = False,
-								target="prime_cat")
-# print("imported")
-# sys.exit()
+if target == "prime_cat":
+	target_import = "../processed_data/prime_categories_expanded.npy"
+elif target == "price":
+	target_import = "../processed_data/target_expanded.npy"
 
+X, Y_prime, L = load_processed_data(	X_filename = "../processed_data/images_RGB_121_91_expanded.npy",
+										Y_filename = target_import,
+										labels_filename = "../processed_data/labels_expanded.npy",
+										randomize = False,
+										target=target)
+
+if target == "prime_cat":
+	Y = np_utils.to_categorical(Y_prime[0],len(np.unique(Y)))
+elif target =="price":
+	Y = Y_prime
 
 #create train/test/valid splits
 x_train, x_test, y_train, y_test = train_test_split(	X,
@@ -47,29 +58,30 @@ x_test, x_valid, y_test, y_valid = train_test_split(	x_test,
 														random_state=seed)
 
 ### format and normalize images and check shapes prior to ML
-
-x_train = x_train.astype("float32"); x_test = x_test.astype("float32"); x_valid = x_valid.astype("float32")
-x_train=x_train/255; x_test=x_test/255; x_valid=x_valid/255
+x_train = x_train.astype("float32")
+x_test = x_test.astype("float32")
+x_valid = x_valid.astype("float32")
+x_train /=255; x_test/=255; x_valid=/255
 print("xtrain shape:",x_train.shape,"ytrain shape:",y_train.shape);print("xtest shape:",x_test.shape,"ytest shape:",y_test.shape);print("xvalid shape:",x_valid.shape,"yvalid shape:",y_valid.shape);
 
 ### Perform learning curve, collect model, and store performance
 
 learn_sets = [100,250,500,750,1000,1500,2000,4000,y_train.shape[0]]
 
-class_model = class_deep_net_model_1(	image_shape=(channels,img_rows,img_cols),
-										loss="categorical_crossentropy")
-
-trained_model, performance = learning_curve(model = class_model,
-											x_train = x_train,
+trained_model, performance = learning_curve(x_train = x_train,
 											y_train = y_train,
 											x_test = x_test,
 											y_test = y_test,
 											learn_sets = learn_sets,
 											save_directory = "../model_data/",
-											target="prime_cat")
-
-updateCSV(	data_to_update = performance,
-			filename = "../model_data/classification_performance.csv")
+											target=target)
+if target == "prime_cat":
+	performance.insert(-1,"hot_one_encoding index",Y_prime[1])
+	updateCSV(	data_to_update = performance,
+				filename = "../model_data/classification_performance.csv")
+elif target == "price":
+	updateCSV(	data_to_update = performance,
+				filename = "../model_data/price_performance.csv")
 
 
 
